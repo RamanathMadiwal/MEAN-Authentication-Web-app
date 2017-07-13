eb5eba688f043c31eb5d5aaa20534b2796d94546
Ramanath Madiwal <ramanathrm.kumta@gmail.com>
2017-07-13 21:54:00 +0530

    commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://brad:bradmeanauthapp@ds121190.mlab.com:21190/',   //prod
+  database: 'mongodb://SuperAdmin:Pleasewait@ds157702.mlab.com:57702/meanauthapp',    //dev
+  secret: 'yoursecret'
+}
